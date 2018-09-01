c0504d3f02e50c552af3a0edf0200891f2ff6460
khalidhmd <khalidhmd@outlook.com>
2018-09-01 19:29:18 +0200

    initial boiler plate code form FreeCodeCamp

---
+++ server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var dns = require('dns');
+
+var cors = require('cors');
+
+var app = express();
+
+// Basic Configuration 
+var port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/ 
+mongoose.connect(process.env.MONGOLAB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+  
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.post("/api/shorturl/new", function(req, res) {
+  if(req.body) {
+    dns.lookup(url, function(err, address){
+      if(err) {
+        res.json({"error":"invalid URL"});
+      } else {
+        res.json({"original_url":req.url,"short_url":1});
+      }
+    })
+  }
+
+  res.end();
+});
+
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
+});
